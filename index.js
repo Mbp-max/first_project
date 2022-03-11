a19c37e834a8377bd351b5b811592e534ea36138
Mario Brito <mariobrito94@hotmail.com>
2022-03-11 14:54:24 -0500

    Friday 311

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const baseUrl = 'http://localhost:3000'
 let goal_gifs = [];
 let randomgif;
-fetch(`http://localhost:3000/gifs/${Math.floor(Math.random() * 30) + 1 }`)
+fetch(`http://localhost:3000/gifs/`)
 .then(resp => resp.json())
 .then(data => {
-    // goal_gifs = data;
-    // randomgif = randomGoal(data);
-    popGoals(data);
+    goal_gifs = data;
+    randomgif = randomGoal(data);
+    popGoals(randomgif);
 })
 function renderGoals(goals){
     console.log(goals)
@@ -22,4 +22,32 @@ function popGoals(goal){
 function randomGoal(goals){
     let goalRender = goals[Math.floor(Math.random() * goals.length)]
     return goalRender
+}
+const likeButton = document.querySelector("#like-button");
+likeButton.addEventListener("click", () => resfreshPage())
+
+const dislikeButton = document.querySelector("#dislike-button");
+dislikeButton.addEventListener("click", () => deleteGif())
+
+fetch(`http://localhost:3000/captions/${Math.floor(Math.random() * 5) + 1 }`)
+.then(resp => resp.json())
+.then(data => popCaptions(data))
+
+function popCaptions(captions){
+        const caption = document.querySelector('#caption')
+        caption.textContent = captions.caption 
+        caption.alt = captions.id   
+}
+
+function resfreshPage(){
+    document.location.reload()
+}
+
+function deleteGif(gifs){
+    console.log(gifs)
+    // fetch(`http://localhost:3000/gifs/${gifs.id}`, {
+    //     method: 'DELETE',
+    // })
+    // .then(resp => resp.json());
+    // resfreshPage();
 }
