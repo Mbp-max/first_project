572f43cf04e6697a4473105aff3522c346bbdc96
Mario Brito <mariobrito94@hotmail.com>
2022-03-11 19:56:07 -0500

    Finished buttons and comments

--- index.js
+++ index.js
@@ -5,49 +5,72 @@ fetch(`http://localhost:3000/gifs/`)
 .then(resp => resp.json())
 .then(data => {
     goal_gifs = data;
-    randomgif = randomGoal(data);
-    popGoals(randomgif);
+    randomgif = randomGoal(goal_gifs);
+    displayGoal(randomgif);
+    console.log(goal_gifs)
+    buttons()
 })
-function renderGoals(goals){
-    console.log(goals)
-}
-
-function popGoals(goal){
-    // goals.forEach(goal => {
-        const gif = document.querySelector('#goalgif')
+const gif = document.querySelector('#goalgif')
+const captions = document.querySelector('#caption')
+function displayGoal(goal){
+    // console.log(goal.watched)
+    if (goal.watched == false){
         gif.src = goal.gif 
-        gif.alt = goal.id   
-}
-
-function randomGoal(goals){
-    let goalRender = goals[Math.floor(Math.random() * goals.length)]
-    return goalRender
+        gif.alt = goal.id
+        captions.textContent = goal.caption
+        goal.watched = true 
+        console.log(goal.watched)}
+    else if ( goal.watched == true ){
+        randomGoal(goal_gifs)
+    }
+    likeButton.addEventListener("click", () => { appendGif(goal), buttonFunctionality()})
 }
 const likeButton = document.querySelector("#like-button");
-likeButton.addEventListener("click", () => resfreshPage())
+const laughButton = document.querySelector('#laugh-button')
+const dislikeButton = document.querySelector('#dislike-button')
 
-const dislikeButton = document.querySelector("#dislike-button");
-dislikeButton.addEventListener("click", () => deleteGif())
-
-fetch(`http://localhost:3000/captions/${Math.floor(Math.random() * 5) + 1 }`)
-.then(resp => resp.json())
-.then(data => popCaptions(data))
-
-function popCaptions(captions){
-        const caption = document.querySelector('#caption')
-        caption.textContent = captions.caption 
-        caption.alt = captions.id   
+function randomGoal(array){
+    let gifSelected = array[Math.floor(Math.random() * array.length) + 1]
+    return gifSelected
 }
 
-function resfreshPage(){
-    document.location.reload()
+function buttons(){
+    laughButton.addEventListener('click', () => buttonFunctionality()),
+    dislikeButton.addEventListener('click', () => buttonFunctionality())
 }
 
-function deleteGif(gifs){
-    console.log(gifs)
-    // fetch(`http://localhost:3000/gifs/${gifs.id}`, {
-    //     method: 'DELETE',
-    // })
-    // .then(resp => resp.json());
-    // resfreshPage();
+function buttonFunctionality(){
+    let gifSelected = randomGoal(goal_gifs)
+    displayGoal(gifSelected)
+    console.log("im being clicked")}
+
+function alertFunction(){
+    alert('Thank you for your feedback')
+}
+function appendGif(gif){
+    const gifContainer = document.querySelector("#gif-container")
+    const gifList = document.createElement("div")
+    const gifImg = document.createElement("img")
+    gifImg.src = gif.gif
+    gifList.append(gifImg)
+    gifContainer.append(gifList)
+}
+
+/////COMMENTS///////
+const usernameInput = document.querySelector('#username')
+const commentInput = document.querySelector('#comment-box')
+const commentSection = document.querySelector("#comment-section")
+const commentForm = document.querySelector('#comment-form')
+
+commentForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    commentSectionInput();
+})
+
+function commentSectionInput(){
+    const newCommentContainer = document.createElement('div')
+    const newUsernameContainer = document.createElement('div')
+    newCommentContainer.textContent = commentInput.value
+    newUsernameContainer.textContent = `${usernameInput.value} : `
+    commentSection.append(newUsernameContainer, newCommentContainer)
 }
